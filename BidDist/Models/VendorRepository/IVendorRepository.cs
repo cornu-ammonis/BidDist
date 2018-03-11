bb62c5402536f4863b0c9e460584345dcc7f667b
cornu-ammonis <andrew.christopher.jones@emory.edu>
2018-03-11 15:57:57 -0400

    initial vendor repository (with dummy method)

---
+++ BidDist/Models/VendorRepository/IVendorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BidDist.Models.VendorRepository
+{
+    interface IVendorRepository
+    {
+        IEnumerable<Vendor> ListVendorsBySearchString(String searchString);
+    }
+}
---
+++ BidDist/Models/VendorRepository/VendorRepository.cs
@@ -0,0 +1,23 @@
+﻿using BidDist.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BidDist.Models.VendorRepository
+{
+    public class VendorRepository : IVendorRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public VendorRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Vendor> ListVendorsBySearchString(String searchString)
+        {
+            return _dbContext.Vendors.ToList();
+        }
+    }
+}
