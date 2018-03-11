cf0e033eb0ef6f31d5508b8f0b5f0ab61890d157
cornu-ammonis <andrew.christopher.jones@emory.edu>
2018-03-11 14:26:25 -0400

    initial vendor model

---
+++ BidDist/Models/Vendor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BidDist.Models
+{
+    public class Vendor
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public IList<String> Cities { get; set; }
+        public IList<String> KeyWords { get; set; }
+        public IList<String> ProductCategories { get; set; }
+        public Boolean IsLocal { get; set; }
+    }
+}
