4bf9539922b362dba3c06687e70a17e644dc78de
cornu-ammonis <andrew.christopher.jones@emory.edu>
2018-03-11 17:15:57 -0400

    initial search algorithm implementation

--- BidDist/Models/VendorRepository/VendorRepository.cs
+++ BidDist/Models/VendorRepository/VendorRepository.cs
@@ -1,4 +1,5 @@
 ﻿using BidDist.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,9 +18,36 @@ namespace BidDist.Models.VendorRepository
 
         public IList<Vendor> ListVendorsBySearchString(String searchString, ApplicationUser user)
         {
-            return _dbContext.Vendors.ToList();
+            IList<Vendor> vendors = VendorsToSearchForUser(user);
+
+            String[] searchStrings = searchString.Split(null);
+            Console.WriteLine(searchStrings[0]);
+
+            IEnumerable<Vendor> vendor_query =
+                (from v in _dbContext.Vendors
+                 where v.KeyWords.Any( k => searchStrings.Any(s =>  s.ToLower().Contains( k.Keyword.ToLower() ) || k.Keyword.ToLower().Contains( s.ToLower() )) )
+                 || v.ProductCategories.Any( pc => searchStrings.Any(s => s.ToLower().Contains( pc.ProductCategory.ToLower() ) || pc.ProductCategory.ToLower().Contains( s.ToLower()) ) )
+                 select v
+                 );
+
+            List<Vendor> resultingVendors = new List<Vendor>();
+            foreach (Vendor v in vendor_query)
+            {
+                resultingVendors.Add(v);
+            }
+
+            return resultingVendors;
+
         }
 
+
+        private IList<Vendor> VendorsToSearchForUser(ApplicationUser user)
+        {
+            if (user.Email == "andrewjones232@gmail.com")
+                return (from v in _dbContext.Vendors select v).Include<Vendor, List<VendorKeyword>>(v => v.KeyWords).ToList();
+            else
+                return new List<Vendor>();
+        }
         
     }
 }
