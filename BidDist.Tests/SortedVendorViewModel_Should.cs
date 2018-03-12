f79af4eedb6811fc0454a99096fde4349b3aa594
cornu-ammonis <andrew.christopher.jones@emory.edu>
2018-03-12 14:07:22 -0400

    SortedVendorViewModel test suite

---
+++ BidDist.Tests/SortedVendorViewModel_Should.cs
@@ -0,0 +1,86 @@
+using System;
+using Xunit;
+using BidDist.Models;
+using BidDist.Models.VendorViewModels;
+using System.Collections.Generic;
+
+namespace BidDist.Tests
+{
+    public class SortedVendorViewModel_Should
+    {
+
+   
+
+        
+        [Fact]
+        public void SortedVendorViewModelGeneratesSearchPointFromKeywords()
+        {
+            SortedVendorViewModel svvm = new SortedVendorViewModel(generateVendorForViewModel(), "keyword");
+
+            Assert.Equal(1, svvm.SearchPoints);
+        }
+
+        [Fact]
+        public void SortedVendorViewModelGeneratesSearchPointFromProductCategory()
+        {
+            SortedVendorViewModel svvm = new SortedVendorViewModel(generateVendorForViewModel(), "product");
+
+            Assert.Equal(1, svvm.SearchPoints);
+        }
+
+        [Fact]
+        public void SortedVendorViewModelGeneratesSearchPointFromKeyWordsAndProductCategory()
+        {
+            SortedVendorViewModel svvm = new SortedVendorViewModel(generateVendorForViewModel(), "product and keyword");
+
+            Assert.Equal(4, svvm.SearchPoints);
+        }
+        private bool IsSortedBySearchPoints(List<VendorViewModel> sortedVendorViewModels)
+        {
+
+            for (int i = 0; i < sortedVendorViewModels.Count - 1; i++)
+            {
+                SortedVendorViewModel sortedVendorViewModelN = (SortedVendorViewModel)sortedVendorViewModels[i];
+                SortedVendorViewModel sortedVendorViewModelNPlusOne = (SortedVendorViewModel)sortedVendorViewModels[i + 1];
+
+                if (sortedVendorViewModelN.SearchPoints < sortedVendorViewModelNPlusOne.SearchPoints)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private Vendor generateVendorForViewModel()
+        {
+            var cities = new List<VendorCity>
+            {
+                new VendorCity {City = "Detroit"}
+            };
+
+           
+
+            var keyWordsOne = new List<VendorKeyword>
+            {
+                new VendorKeyword { Keyword = "Keyword"},
+                new VendorKeyword { Keyword = "And" },
+                new VendorKeyword { Keyword = "Bolts"}
+            };
+
+          
+
+            var productsOne = new List<VendorProductCategory>
+            {
+                new VendorProductCategory { ProductCategory = "Product"},
+                new VendorProductCategory { ProductCategory = "And"},
+                new VendorProductCategory {ProductCategory = "Nails"}
+            };
+
+
+
+            var vendor = new Vendor { Name = "vendor one", Email = "vendor1@demo.com", IsLocal = true, Cities = cities, KeyWords = keyWordsOne, ProductCategories = productsOne };
+
+            return vendor;
+            
+        }
+    }
+}
