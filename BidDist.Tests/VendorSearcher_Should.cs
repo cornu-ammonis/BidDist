e0d72682685ab2c0d029c32b9eb4a0e44b8d8987
cornu-ammonis <andrew.christopher.jones@emory.edu>
2018-03-12 15:14:23 -0400

    test vendor searcher returns partial match by product

--- BidDist.Tests/VendorSearcher_Should.cs
+++ BidDist.Tests/VendorSearcher_Should.cs
@@ -19,11 +19,21 @@ namespace BidDist.Tests
         [Fact]
         public void VendorSearcherSearchVendorsByStringReturnsPartialMatchForKeyword()
         {
-            IList<Vendor> testListPartial = VendorSearcher.SearchVendorsByString(vendors, "key");
+            IList<Vendor> testListPartial = VendorSearcher.SearchVendorsByString(vendors, "key abcdefg");
 
             bool validMatch = testListPartial.Count == 1 && testListPartial[0].KeyWords[0].Keyword.ToLower().Contains("key");
 
             Assert.True(validMatch, "vendor searcher search vendor by string should return a valid match according to keyword");
+        }
+
+        [Fact]
+        public void VendorSearcherSearchVendorsByStringReturnsPartialMatchForProduct()
+        {
+            IList<Vendor> testListPartial = VendorSearcher.SearchVendorsByString(vendors, "prod abd");
+
+            bool validMatch = testListPartial.Count == 1 && testListPartial[0].ProductCategories[0].ProductCategory.ToLower().Contains("prod");
+
+            Assert.True(validMatch, "vendor searcher search vendor by string should return a valid match according to product");
         }
     }
 }
