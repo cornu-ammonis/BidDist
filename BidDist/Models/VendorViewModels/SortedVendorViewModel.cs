6e67f3d4e8348969cb2ce3efd6dc8559ff758d74
cornu-ammonis <andrew.christopher.jones@emory.edu>
2018-03-11 20:53:34 -0400

    add points for keywords

--- BidDist/Models/VendorViewModels/SortedVendorViewModel.cs
+++ BidDist/Models/VendorViewModels/SortedVendorViewModel.cs
@@ -7,12 +7,15 @@ namespace BidDist.Models.VendorViewModels
 {
     public class SortedVendorViewModel : VendorViewModel
     {
-        public int SearchPoints { get; set; } = 0;
+        public int SearchPoints { get; set; }
 
         public SortedVendorViewModel(Vendor vendor, String searchString) : base(vendor) 
         {
             String[] searchStrings = searchString.ToLower().Split();
+            SearchPoints = 0;
             AddPointsForProductCategory(searchStrings, vendor.ProductCategories);
+            AddPointsForKeywords(searchStrings, vendor.KeyWords);
+            ProductCategories = SearchPoints.ToString();
         }
 
         private void AddPointsForProductCategory(String [] searchStrings, List<VendorProductCategory> vpcs)
@@ -32,6 +35,33 @@ namespace BidDist.Models.VendorViewModels
                     if (!excludeString[i])
                     {
                         if ( searchStrings[i].Contains(productCategory) || productCategory.Contains(searchStrings[i]) )
+                        {
+                            SearchPoints += 1;
+                            excludeString[i] = true;
+                            excluded++;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void AddPointsForKeywords(String[] searchStrings, List<VendorKeyword> vendorKeywords)
+        {
+            Boolean[] excludeString = new Boolean[searchStrings.Length];
+            int excluded = 0;
+
+            foreach (VendorKeyword vendorKeyword in vendorKeywords)
+            {
+                if (excluded == excludeString.Length)
+                    break;
+
+                String keyword = vendorKeyword.Keyword.ToLower();
+
+                for (int i = 0; i < searchStrings.Length; i++)
+                {
+                    if (!excludeString[i])
+                    {
+                        if (searchStrings[i].Contains(keyword) || keyword.Contains(searchStrings[i]))
                         {
                             SearchPoints += 1;
                             excludeString[i] = true;
