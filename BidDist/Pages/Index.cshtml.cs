1ba4b03c440d74ad7360493a9626c52c16bec724
cornu-ammonis <andrew.christopher.jones@emory.edu>
2018-03-13 14:50:48 -0400

    dummy file download

--- BidDist/Pages/Index.cshtml.cs
+++ BidDist/Pages/Index.cshtml.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using BidDist.Data;
 using BidDist.Models;
 using BidDist.Models.VendorRepository;
 using BidDist.Models.VendorViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -38,6 +41,13 @@ namespace BidDist.Pages
             }
             else
                 VendorViewModels = new ListSortedVendorViewModel(new List<Vendor>(), "").VendorViewModels;
+        }
+
+        public async Task<IActionResult> OnGetExportAsync()
+        {
+            String test = "a, b, c, d, e, f, g";
+
+            return File(new MemoryStream(Encoding.UTF8.GetBytes(test)), "text/csv", "test.csv");
         }
     }
 }
