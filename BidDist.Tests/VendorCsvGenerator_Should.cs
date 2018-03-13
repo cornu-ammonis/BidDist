236a5aa834fac21507e39a6fa78552424360d40e
cornu-ammonis <andrew.christopher.jones@emory.edu>
2018-03-13 15:16:12 -0400

    two unit tests for csv generator

---
+++ BidDist.Tests/VendorCsvGenerator_Should.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+using BidDist.Models;
+using BidDist.Models.VendorViewModels;
+using System.Collections.Generic;
+using BidDist.Models.VendorRepository;
+
+namespace BidDist.Tests
+{
+    public class VendorCsvGenerator_Should
+    {
+        private List<VendorViewModel> vendorViewModels;
+
+        public VendorCsvGenerator_Should()
+        {
+            vendorViewModels = VendorListGenerator.generateVendorViewModelList();
+        }
+
+        [Fact]
+        public void VendorCsvGeneratorShouldCreateARowForEachVendorInput()
+        {
+           String csv = VendorsCsvGenerator.GenerateVendorCsv(vendorViewModels);
+
+            bool rowsEqualsViewModelCountPlusOne = (csv.Split().Length == vendorViewModels.Count + 1);
+
+            Assert.True(rowsEqualsViewModelCountPlusOne, "vendor csv generator should return a string with number of rows equal to inputted vendors + 1");
+        }
+
+        [Fact]
+        public void VendorCsvGeneratorShouldCreateHeaderRowAndSubsequentRowsOfEqualLength()
+        {
+            String[] csv = VendorsCsvGenerator.GenerateVendorCsv(vendorViewModels).Split();
+
+            bool eachRowIsEqualLength = csv.Length >= 2;
+
+            int headerLength = csv[0].Split(',').Length;
+
+            for (int i = 1; i < csv.Length; i++)
+                if (csv[i].Split(',').Length != headerLength)
+                    eachRowIsEqualLength = false;
+
+            Assert.True(eachRowIsEqualLength, "Vendor Csv Generator should return a csv where column header and row length is equal");
+
+        }
+
+       
+    }
+}
---
+++ BidDist/Models/VendorRepository/VendorsCsvGenerator.cs
@@ -0,0 +1,16 @@
+﻿using BidDist.Models.VendorViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BidDist.Models.VendorRepository
+{
+    public static class VendorsCsvGenerator
+    {
+        public static String GenerateVendorCsv(List<VendorViewModel> vendorViewModels)
+        {
+            return null;
+        }
+    }
+}
