e56286625e61a7d0e416188c63564f589edf7757
cornu-ammonis <andrew.christopher.jones@emory.edu>
2018-03-18 13:21:53 -0400

    file upload on admin page

--- BidDist/Pages/Admin.cshtml.cs
+++ BidDist/Pages/Admin.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,18 @@ namespace BidDist.Pages
 {
     public class AdminModel : PageModel
     {
+        [BindProperty]
+        public IFormFile FileUpload { get; set; }
+        public String TestString { get; set; } = "no";
         public void OnGet()
         {
-
+          
+        }
+        
+        public void OnPost()
+        {
+            if (FileUpload != null)
+                TestString = FileUpload.ToString();
         }
     }
 }
