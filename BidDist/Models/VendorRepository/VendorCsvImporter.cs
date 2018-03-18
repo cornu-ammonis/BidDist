de93d1207854eaa3d6f94e179a4fbef4d9df5b8a
cornu-ammonis <andrew.christopher.jones@emory.edu>
2018-03-18 13:57:04 -0400

    unit test for VendorCsvImporter

---
+++ BidDist/Models/VendorRepository/VendorCsvImporter.cs
@@ -0,0 +1,40 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BidDist.Models.VendorRepository
+{
+    public static class VendorCsvImporter
+    {
+        public static List<Vendor> VendorCsvToListOfVendors(StreamReader vendorCsv)
+        {
+            List<string[]> rows = new List<string[]>();
+
+            var parser = new CsvParser(vendorCsv);
+            while (true)
+            {
+                string[] row = parser.Read();
+                if (row == null)
+                {
+                    break;
+                }
+
+                rows.Add(row);
+            }
+
+            int nameColumn = 0;
+            int cityColumn = 2;
+            int keywordsColumn = 4;
+            int descriptionColumn = 5;
+
+            List<Vendor> vendors = new List<Vendor>();
+
+
+
+            return vendors;
+        }
+    }
+}
