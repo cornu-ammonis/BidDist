44a1ea5e9d0acef6eb2b9900cb4608458a68ed40
cornu-ammonis <andrew.christopher.jones@emory.edu>
2018-03-21 18:39:20 -0400

    csv export functionality

--- BidDist/Pages/Admin.cshtml.cs
+++ BidDist/Pages/Admin.cshtml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BidDist.Models;
+using BidDist.Models.VendorRepository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,7 +15,14 @@ namespace BidDist.Pages
     {
         [BindProperty]
         public IFormFile FileUpload { get; set; }
-        public String TestString { get; set; } = "no";
+        public String TestString { get; set; } = "";
+        private readonly IVendorRepository _vendorRepository;
+
+        public AdminModel(IVendorRepository vendorRepository)
+        {
+            _vendorRepository = vendorRepository;
+        }
+
         public void OnGet()
         {
           
@@ -21,7 +31,15 @@ namespace BidDist.Pages
         public void OnPost()
         {
             if (FileUpload != null)
-                TestString = FileUpload.ToString();
+            {
+                using (var reader = new StreamReader(FileUpload.OpenReadStream()))
+                {
+                    List<Vendor> vendors = VendorCsvImporter.VendorCsvToListOfVendors(reader);
+                    int newAdded = _vendorRepository.AddVendorListToDatabase(vendors);
+                    TestString = newAdded.ToString() + " new vendors imported to the database! (out of " + vendors.Count.ToString() + " in the file)";
+                }
+
+            }
         }
     }
 }
